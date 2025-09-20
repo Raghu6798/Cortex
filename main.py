5b0de5a80adc1d48e6878ea7f355aff9df3c3dd5
Raghu6798 <courage9605@gmail.com>
2025-09-20 12:04:55 +0530

    switched back to non-streaming inference

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain.agents import create_agent
 from langgraph.checkpoint.memory import InMemorySaver
 
 from dotenv import load_dotenv
-
+import asyncio
 load_dotenv()
 @tool
 def search(query: str) -> str:
@@ -29,10 +29,18 @@ checkpointer = InMemorySaver()
 agent = create_agent(model=llm,tools = tools,prompt = "You are a helpful AI assistant",checkpointer=checkpointer,)
 
 thread_id = str(uuid4())
-for token, metadata in agent.stream(
-    {"messages": [{"role": "user", "content": "What are your capabilities in detail"}]},
-    stream_mode="messages",
-    config = {"configurable": {"thread_id": thread_id}},
-):
 
-    print(token.content,end = "",flush=True)
+# --- Async runner ---
+async def run_stream():
+    async for event in agent.astream(
+        {"messages": [{"role": "user", "content": "What are your capabilities in detail"}]},
+        stream_mode="messages",
+        config={"configurable": {"thread_id": thread_id}},
+    ):   
+        chunk, metadata = event
+        print(chunk.content,end = "",flush=True)
+
+
+if __name__ == "__main__":
+    asyncio.run(run_stream())
+
