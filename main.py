989e31e2314f69f2eeccfc26ca1f5b1bda6fe59e
Raghu6798 <courage9605@gmail.com>
2025-10-19 12:07:44 +0530

    removed trailing slash for the allowed_origins in CORSMiddleware

--- main.py
+++ main.py
@@ -62,7 +62,10 @@ app = FastAPI(lifespan=lifespan)
 # --- Middleware ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cortexagents.netlify.app/","http://localhost:3000"], 
+    allow_origins=[
+        "https://cortexagents.netlify.app",
+        "http://localhost:3000"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
